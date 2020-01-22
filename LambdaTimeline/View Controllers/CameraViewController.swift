3fe77f0c80211293385d7658979a35e9ca0389f7
Vici Shaweddy <vshaweddy@gmail.com>
2020-01-22 09:06:33 -0500

    live preview works

--- LambdaTimeline/View Controllers/CameraViewController.swift
+++ LambdaTimeline/View Controllers/CameraViewController.swift
@@ -12,28 +12,97 @@ import AVFoundation
 class CameraViewController: UIViewController {
     
     @IBOutlet weak var recordButton: UIButton!
-    @IBOutlet weak var cameraView: UIView!
+    @IBOutlet weak var cameraView: CameraPreviewView!
     
     lazy private var captureSession = AVCaptureSession()
     lazy private var fileOutput = AVCaptureMovieFileOutput()
     var player: AVPlayer!
+//    var playerView: PlaybackView!
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        self.setUpCamera()
 
         // Do any additional setup after loading the view.
     }
     
-    override var prefersStatusBarHidden: Bool {
-        return true
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        captureSession.startRunning()
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        captureSession.stopRunning()
+    }
+    
+    private func setUpCamera() {
+        let camera = bestCamera()
+        captureSession.beginConfiguration()
+        
+        guard let cameraInput = try? AVCaptureDeviceInput(device: camera) else {
+            fatalError("Can't create an input form the camera, do something better than crashing")
+        }
+        
+        // Add camera inputs
+        guard captureSession.canAddInput(cameraInput) else {
+            fatalError("This session can't handle this type of input: \(cameraInput)")
+        }
+        captureSession.addInput(cameraInput)
+        
+        if captureSession.canSetSessionPreset(.hd1920x1080) {
+            captureSession.sessionPreset = .hd1920x1080
+        }
+        
+        // Add audio inputs
+        let microphone = bestAudio()
+        guard let audioInput = try? AVCaptureDeviceInput(device: microphone) else {
+            fatalError("Can't create input from microphone")
+        }
+        
+        guard captureSession.canAddInput(audioInput) else {
+            fatalError("Can't add audio input")
+        }
+        
+        // Add video output (save movie)
+        guard captureSession.canAddOutput(fileOutput) else {
+            fatalError("Can't record to disk")
+        }
+        
+        captureSession.addOutput(fileOutput)
+        captureSession.commitConfiguration()
+        cameraView.session = captureSession
+    }
+
+    private func bestCamera() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(.builtInUltraWideCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        if let device = AVCaptureDevice.default(.builtInWideAngleCamera, for: .video, position: .back) {
+            return device
+        }
+        
+        fatalError("No cameras on the device (ir running it on the iPhone simulator)")
+    }
+    
+    private func bestAudio() -> AVCaptureDevice {
+        if let device = AVCaptureDevice.default(for: .audio) {
+            return device
+        }
+        fatalError("No audio")
     }
     
     @IBAction func cancelPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
     
-
+    @IBAction func recordPressed(_ sender: Any) {
+    }
+    
     /*
     // MARK: - Navigation
 
---
+++ LambdaTimeline/Views/CameraPreviewView.swift
@@ -0,0 +1,26 @@
+//
+//  CameraPreviewView.swift
+//  LambdaTimeline
+//
+//  Created by Vici Shaweddy on 1/22/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class CameraPreviewView: UIView {
+    
+    override class var layerClass: AnyClass {
+        return AVCaptureVideoPreviewLayer.self
+    }
+    
+    var videoPlayerView: AVCaptureVideoPreviewLayer {
+        return layer as! AVCaptureVideoPreviewLayer
+    }
+    
+    var session: AVCaptureSession? {
+        get { return videoPlayerView.session }
+        set { videoPlayerView.session = newValue }
+    }
+}
